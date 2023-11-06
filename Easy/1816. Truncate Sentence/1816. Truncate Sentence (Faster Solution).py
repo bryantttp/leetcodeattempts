cb730a67c003e007d1750fb67376382e6ea4ab33
bryantttp <149286642+bryantttp@users.noreply.github.com>
2023-11-06 20:04:26 +0800

    Update 1816. Truncate Sentence (Faster Solution).py

--- Easy/1816. Truncate Sentence/1816. Truncate Sentence (Faster Solution).py
+++ Easy/1816. Truncate Sentence/1816. Truncate Sentence (Faster Solution).py
@@ -1,7 +1,7 @@
 class Solution(object):
     def truncateSentence(self, s, k):
-        new_s = s.split()
-        output = ' '.join(new_s[0:k])
+        new_s = s.split()  # Function creates a list and stores each string into list
+        output = ' '.join(new_s[0:k])  # Function returns a string with each element up til the kth element with a space in between each other
         return output
         # """
         # :type s: str
